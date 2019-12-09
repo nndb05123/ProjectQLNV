469ec5bf0ac8d0bffc695f3b5652f7c82a85e726
nndb05123 <duybao05123@gmail.com>
2019-12-09 14:22:07 +0700

    update DAO, BUS

---
+++ Final Project/DATN_SourceCode/HRM_VTHP/Core/DAO/DanTocDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM_VTHP.Core.DAO
+{
+    class DanTocDAO
+    {
+    }
+}
